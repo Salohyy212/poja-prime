9f9e0fa79155786349f6c12be9da2fd1f0bdecfc
Salohyy212 <zerambaysalohy@gmail.com>
2023-12-12 20:00:58 +0100

    fix: arrange files

---
+++ src/main/java/school/hei/poja/endpoint/rest/controller/PrimeController.java
@@ -0,0 +1,16 @@
+package school.hei.poja.endpoint.rest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+@Controller
+public class PrimeController {
+    @GetMapping("new-prime")
+    public BigInteger generatePrime(){
+        BigInteger primeNumber = BigInteger.probablePrime(10000, new Random());
+        return primeNumber;
+    }
+}
