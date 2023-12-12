0812bb671fc692c45c8f446ff859050a2ae6956a
Salohyy212 <zerambaysalohy@gmail.com>
2023-12-12 20:43:24 +0100

    fix: arrangement

--- src/main/java/mg/hei/prime/file/FileHash.java
+++
@@ -1,6 +0,0 @@
-package mg.hei.prime.file;
-
-import mg.hei.prime.PojaGenerated;
-
-@PojaGenerated
-public record FileHash(FileHashAlgorithm algorithm, String value) {}
--- src/main/java/mg/hei/prime/file/FileHashAlgorithm.java
+++
@@ -1,8 +0,0 @@
-package mg.hei.prime.file;
-
-import mg.hei.prime.PojaGenerated;
-
-@PojaGenerated
-public enum FileHashAlgorithm {
-  SHA256
-}
